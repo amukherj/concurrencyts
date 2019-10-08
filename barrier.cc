06fc4e6eef3aed73cca2e9a8ec58384a38cbaccc
amukherj <arindam.mukerjee@gmail.com>
2019-10-08 18:13:47 +0530

    Added support for flex_barrier, which is identical to barrier except for allowing execution of some custom code once the barrier is released and before it is reset.

--- barrier.cc
+++ barrier.cc
@@ -4,35 +4,12 @@
 #include <iostream>
 #include <thread>
 
-namespace concurrencyts {
-
-barrier::barrier(size_t count) : max_count{count}, count{count}, released_count{0} {}
-
-void barrier::arrive_and_wait() {
-  std::unique_lock<std::mutex> ul{lock};
-  allow.wait(ul, [this]()->bool { return count > 0; });
-
-  if (--count == 0) {
-    ul.unlock();
-    done.notify_all();
-  } else {
-    done.wait(ul, [this]()->bool { return count == 0; });
-    ul.unlock();
-  }
-
-  ul.lock();
-  if (++released_count == max_count) {
-    count = max_count;
-    released_count = 0;
-    ul.unlock();
-    allow.notify_all();
-  }
-}
-
-}
 
 int main() {
-  concurrencyts::barrier barrier(7);
+  concurrencyts::flex_barrier barrier(7, []() {
+    std::cout << "All threads done\n";
+  });
+
   for (int i = 0; i < 6; ++i) {
     std::thread thr([&, i]() {
       std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -57,6 +34,5 @@ int main() {
     thr.detach();
   }
   barrier.arrive_and_wait();
-  std::cout << "All threads done\n";
   std::this_thread::sleep_for(std::chrono::seconds(5));
 }
--- barrier.h
+++ barrier.h
@@ -6,10 +6,36 @@
 
 namespace concurrencyts {
 
-class barrier {
+template <typename T>
+class barrier_base {
 public:
-  barrier(size_t count);
-  void arrive_and_wait();
+  typedef void(*void_func)();
+  barrier_base(size_t count) : max_count{count}, count{count}, released_count{0} {}
+
+  void arrive_and_wait() {
+    std::unique_lock<std::mutex> ul{lock};
+    allow.wait(ul, [this]()->bool { return count > 0; });
+  
+    if (--count == 0) {
+      ul.unlock();
+      done.notify_all();
+    } else {
+      done.wait(ul, [this]()->bool { return count == 0; });
+      ul.unlock();
+    }
+  
+    ul.lock();
+    if (++released_count == max_count) {
+      count = max_count;
+      released_count = 0;
+      ul.unlock();
+      auto ffunc = static_cast<T*>(this)->final_func();
+      if (ffunc) {
+        ffunc();
+      }
+      allow.notify_all();
+    }
+  }
 
 private:
   const size_t max_count;
@@ -18,6 +44,27 @@ private:
   std::condition_variable done;
   size_t count;
   size_t released_count;
+};
+
+class barrier : public barrier_base<barrier> {
+public:
+  barrier(size_t count) : barrier_base{count} {}
+
+  void_func final_func() {
+    return nullptr;
+  }
+};
+
+class flex_barrier : public barrier_base<flex_barrier> {
+public:
+  flex_barrier(size_t count, void_func fnl_func) : barrier_base{count}, fnl_func{fnl_func} {}
+
+  void_func final_func() {
+    return fnl_func;
+  }
+
+private:
+  void_func fnl_func;
 };
 
 }
